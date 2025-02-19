f1c87e7e0e5493baf448e5331597b01a033028ef
Rohit Kokani <148274186+Rohitkokanii@users.noreply.github.com>
2025-02-19 15:31:43 +0530

    added

--- Q1_2.c
+++ Q1_2.c
@@ -1,17 +1,28 @@
 #include<stdio.h>
 
 int main() {
-	int min, max;
-	int num;
-	printf("Enter Min Max = ");
-	scanf_s("%d%d", &min, &max);
-
-	for (int i=min;i<=max;i++) {
-		
-
-	}
+	int min,max;
+	printf("\nEnter Min -> Max No. = ");
+	scanf_s("%d%d", &min,&max);
 
 	
+	int lastDigit;
+	int rev = 0;
+
+	for(int i=min;i<=max;i++){
+		int num = i;
+		int orgNum = i;
+		while (num > 0) {
+			lastDigit = num % 10;
+			rev = rev * 10 + lastDigit;
+			num /= 10;
+		}
+
+		if (rev == orgNum) {
+			printf("\nPalindrom = %d\n", rev);
+		}
+		rev = 0;
+	}
 
 	return 0;
 }
