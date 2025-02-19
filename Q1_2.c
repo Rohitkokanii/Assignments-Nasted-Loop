792e1c0cadd0902a73df326040f61c71c007ed5e
Rohit Kokani <148274186+Rohitkokanii@users.noreply.github.com>
2025-02-19 14:22:15 +0530

    Add project files.

---
+++ Q1_2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+	int min, max;
+	int num;
+	printf("Enter Min Max = ");
+	scanf_s("%d%d", &min, &max);
+
+	for (int i=min;i<=max;i++) {
+		
+
+	}
+
+	
+
+	return 0;
+}
